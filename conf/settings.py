06256bc48d2330d69e685e04506aec0305a8e888
jinyinglee <45194511+jinyinglee@users.noreply.github.com>
2021-11-22 12:10:16 +0800

    update swagger login settings

--- conf/settings.py
+++ conf/settings.py
@@ -128,3 +128,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+
+
+SWAGGER_SETTINGS = {
+   'USE_SESSION_AUTH': False
+}
