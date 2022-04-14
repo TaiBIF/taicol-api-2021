b9346572183068e52964525109b4f6fc0041a6ab
jinyinglee <45194511+jinyinglee@users.noreply.github.com>
2022-04-14 17:42:27 +0800

    add: generate formatted name

--- scripts/generate-taxonID.py
+++ scripts/generate-taxonID.py
@@ -1,5 +1,7 @@
 # generate taxonID for the first time
 
+import itertools
+from importlib.abc import TraversableResources
 from sys import dllhandle
 from unittest import result
 from conf.settings import env
@@ -16,6 +18,8 @@ db_settings = {
     "password": env('DB_PASSWORD'),
     "db": env('DB_DBNAME'),
 }
+
+conn = pymysql.connect(**db_settings)
 
 
 def get_related_names(taxon_name_id, rank_id, df, new_names):
@@ -42,9 +46,6 @@ def get_related_names(taxon_name_id, rank_id, df, new_names):
         names = [l[0] for l in names]
         new_names += [n for n in names if n not in list(df.taxon_name_id)]
     return new_names, df
-
-
-conn = pymysql.connect(**db_settings)
 
 
 # add publish_date to references table
@@ -92,6 +93,7 @@ for r in results[results['publish_date'] != ''].index:
 #          tn.rank_id FROM reference_usages ru \
 #          JOIN taxon_names tn ON tn.id = ru.taxon_name_id \
 #          WHERE JSON_EXTRACT(ru.properties, "$.is_in_taiwan") = 1 AND ru.status NOT IN ("", "undetermined")'
+# 沒有在台灣 & 沒有在reference_usages裡面的資料不給taxonID
 query = 'SELECT ru.id, ru.reference_id, ru.`group`, ru.taxon_name_id, tn.rank_id  FROM reference_usages ru \
          INNER JOIN taxon_names tn ON ru.taxon_name_id = tn.id  \
          WHERE JSON_EXTRACT(ru.properties, "$.is_in_taiwan") = 1 AND ru.status NOT IN ("", "undetermined")'
@@ -167,14 +169,14 @@ with conn.cursor() as cursor:
 
 total_df = total_df.merge(results)
 
-query = f'SELECT id, status FROM reference_usages \
+query = f'SELECT id, status, taxon_name_id, parent_taxon_name_id FROM reference_usages \
           WHERE id IN {tuple(total_df.ru_id.unique())}'
 
 with conn.cursor() as cursor:
     cursor.execute(query)
     results = cursor.fetchall()
     results = pd.DataFrame(results)
-    results = results.rename(columns={0: 'ru_id', 1: 'status'})
+    results = results.rename(columns={0: 'ru_id', 1: 'status', 2: 'taxon_name_id', 3: 'parent_taxon_name_id'})
 
 total_df = total_df.merge(results)
 
@@ -296,7 +298,7 @@ for i in total_df[(total_df['is_latest']) & (total_df['taxon_status'] == 'accept
             is_brackish = results.get('is_brackish')
             is_marine = results.get('is_marine')
         # 寫入table
-        query = f"INSERT INTO taxon (taxon_id, rank_id, accepted_taxon_name_id, common_name_c, alternative_name_c,  \
+        query = f"INSERT INTO api_taxon (taxon_id, rank_id, accepted_taxon_name_id, common_name_c, alternative_name_c,  \
                     is_hybrid, is_endemic, alien_type, is_fossil, is_terrestrial, is_freshwater, is_brackish, is_marine ) \
                     VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
         with conn.cursor() as cursor:
@@ -309,15 +311,213 @@ for i in total_df.index:
     print('now', i)
     row = total_df.iloc[i]
     is_latest = 1 if row.is_latest else 0
-    query = f"INSERT INTO taxon_usages (reference_usage_id, reference_id, taxon_id, status, is_latest) \
+    query = f"INSERT INTO api_taxon_usages (reference_usage_id, reference_id, taxon_id, status, is_latest, taxon_name_id, parent_taxon_name_id) \
                 VALUES (%s, %s, %s, %s, %s)"
     with conn.cursor() as cursor:
-        cursor.execute(query, (row.ru_id, row.reference_id, row.taxon_id, row.taxon_status, is_latest))
+        cursor.execute(query, (row.ru_id, row.reference_id, row.taxon_id, row.taxon_status, is_latest, taxon_name_id, parent_taxon_name_id))
         conn.commit()
 
-# 寫入taxon_hierarchy table
-# 從reference_usages一層一層抓parent
-h_df = pd.DataFrame()
-for ru in total_df[(total_df['is_latest']) & (total_df['taxon_status'] == 'accepted')].ru_id:
 
-    # 會有上層對到的name不是taxon的accepted name嗎?
+# 寫入taxon_hierarchy table
+# 1. 從 taxon_usages 裡面抓 is_latest = 1 & status = accepted
+# 2. 將該taxon_id存入hierarchy table (parent & child都是自己)
+# 3. 抓對應到reference_usages裡面的parent_taxon_name_id
+# 4. 抓該parent_taxon_name_id對應到的taxon_id存進hierarchy table(parent是parent_taxon_name_id對到的, child是自己) -> 這樣好像只會存到length=1的組合
+
+# sandy 寫法
+# 此寫法是串name，要改成串taxon
+# WITH RECURSIVE find_ancestor (taxon_name_id, usage_id, reference_id, path, root_taxon_name_id) AS
+#     (
+#         SELECT taxon_name_id, usage_id, reference_id, cast(taxon_name_id as CHAR(50)) as path, taxon_name_id as root_taxon_name_id
+#         FROM accepted_usages
+#         WHERE parent_taxon_name_id IS NULL
+#         UNION ALL
+#         SELECT c.taxon_name_id, c.usage_id, c.reference_id, concat(cast(c.taxon_name_id as CHAR(50)) , '>',  path), root_taxon_name_id
+#         FROM find_ancestor AS cp
+#         JOIN accepted_usages AS c
+#             ON cp.taxon_name_id = c.parent_taxon_name_id
+#     )
+#     SELECT find_ancestor.*, taxon_names.name, taxon_names.rank_id
+#     FROM find_ancestor
+#     left join taxon_names on taxon_names.id = find_ancestor.root_taxon_name_id
+#     where taxon_name_id in ({$taxonNamesIdString}) and taxon_names.rank_id = 3
+
+# ------
+
+query = f"""
+WITH RECURSIVE find_ancestor (rank_id, taxon_name_id, reference_usage_id, reference_id, path, root_taxon_name_id, path2) AS
+    (
+        SELECT t.rank_id, c.taxon_name_id, c.reference_usage_id, c.reference_id, cast(c.taxon_id as CHAR(1000)) as path, c.taxon_name_id as root_taxon_name_id , cast(c.taxon_name_id as CHAR(1000)) as path2 
+        FROM api_taxon_usages c
+        JOIN taxon_names AS t
+            on c.taxon_name_id = t.id 
+        WHERE c.parent_taxon_name_id IS NULL and c.is_latest = 1 and c.`status` = 'accepted' and t.rank_id <= 34
+        UNION ALL
+        SELECT t.rank_id, c.taxon_name_id, c.reference_usage_id, c.reference_id, concat(cast(c.taxon_id as CHAR(1000)) , '>',  path), root_taxon_name_id, concat(cast(c.taxon_name_id as CHAR(1000)) , '>',  path2) 
+        FROM find_ancestor AS cp
+        JOIN api_taxon_usages AS c
+            ON cp.taxon_name_id = c.parent_taxon_name_id
+        JOIN taxon_names AS t
+            on c.taxon_name_id = t.id 
+        WHERE c.is_latest = 1 and c.status = 'accepted' and t.rank_id <= 34
+    )
+    SELECT *
+    FROM find_ancestor
+"""
+
+with conn.cursor() as cursor:
+    cursor.execute(query)
+    results = cursor.fetchall()
+
+df = pd.DataFrame()
+for r in results:
+    print(r[2])
+    t_list = r[4].split('>')
+    for i in itertools.combinations_with_replacement(t_list, 2):
+        if not(i[0] == i[1] and i[0] != t_list[0]):
+            length = t_list.index(i[1]) - t_list.index(i[0])
+            df = df.append({'parent_taxon_id': i[1], 'child_taxon_id': i[0], 'length': length}, ignore_index=True)
+    df = df.drop_duplicates()  # 移除重複的組合
+    # 先確認組合是不是已經存在
+    # conn = pymysql.connect(**db_settings)
+    #         query = f"INSERT INTO taxon_hierarchy (parent_taxon_id, child_taxon_id, length) \
+    #                     SELECT * FROM (SELECT %s as parent_taxon_id, %s as child_taxon_id, %s) AS tmp \
+    #                     WHERE NOT EXISTS ( \
+    #                         SELECT id FROM taxon_hierarchy WHERE parent_taxon_id=%s and child_taxon_id=%s and length=%s \
+    #                     ) LIMIT 1;"
+    #         with conn.cursor() as cursor:
+    #             cursor.execute(query, (i[1], i[0], length, i[1], i[0], length))
+    #             conn.commit()
+
+
+# TODO 種下＆種下下階層另外處理
+# 如果有種下的種階層是無效名，那該種下就是「單獨有Taxon_id 但沒有上階層」
+# 確認種下的上階層是否有效
+
+query = "WITH RECURSIVE find_ancestor (rank_id, taxon_name_id, reference_usage_id, reference_id, path, root_taxon_name_id, path2) AS \
+    ( \
+        SELECT t.rank_id, c.taxon_name_id, c.reference_usage_id, c.reference_id, cast(c.taxon_id as CHAR(1000)) as path, c.taxon_name_id as root_taxon_name_id , cast(c.taxon_name_id as CHAR(1000)) as path2 \
+        FROM api_taxon_usages c \
+        JOIN taxon_names AS t \
+            on c.taxon_name_id = t.id \
+        WHERE c.parent_taxon_name_id IS NULL and c.is_latest = 1 and c.`status` = 'accepted' \
+        UNION ALL \
+        SELECT t.rank_id, c.taxon_name_id, c.reference_usage_id, c.reference_id, concat(cast(c.taxon_id as CHAR(1000)) , '>',  path), root_taxon_name_id, concat(cast(c.taxon_name_id as CHAR(1000)) , '>',  path2) \
+        FROM find_ancestor AS cp \
+        JOIN api_taxon_usages AS c \
+            ON cp.taxon_name_id = c.parent_taxon_name_id \
+        JOIN taxon_names AS t \
+            on c.taxon_name_id = t.id \
+        WHERE c.is_latest = 1 and c.status = 'accepted' \
+    ) \
+    SELECT * \
+    FROM find_ancestor \
+    WHERE taxon_name_id IN ( \
+        SELECT t1.taxon_name_id FROM api_taxon_usages t1, api_taxon_usages t2 \
+        JOIN taxon_names t ON t.id = t2.taxon_name_id \
+        WHERE t1.parent_taxon_name_id = t2.taxon_name_id and t2.status = 'accepted' and t.rank_id >= 34);"
+
+with conn.cursor() as cursor:
+    cursor.execute(query)
+    results = cursor.fetchall()
+
+for r in results:
+    print(r[2])
+    conn = pymysql.connect(**db_settings)
+    t_list = r[4].split('>')
+    for i in itertools.combinations_with_replacement(t_list, 2):
+        if not(i[0] == i[1] and i[0] != t_list[0]):
+            length = t_list.index(i[1]) - t_list.index(i[0])
+            # 先確認組合是不是已經存在
+            query = f"INSERT INTO api_taxon_hierarchy (parent_taxon_id, child_taxon_id, length) \
+                        SELECT * FROM (SELECT %s as parent_taxon_id, %s as child_taxon_id, %s) AS tmp \
+                        WHERE NOT EXISTS ( \
+                            SELECT id FROM api_taxon_hierarchy WHERE parent_taxon_id=%s and child_taxon_id=%s and length=%s \
+                        ) LIMIT 1;"
+            with conn.cursor() as cursor:
+                cursor.execute(query, (i[1], i[0], length, i[1], i[0], length))
+                conn.commit()
+
+query = "SELECT rank_id, nomenclature_id, properties, id FROM taxon_names WHERE rank_id <> 47"
+conn = pymysql.connect(**db_settings)
+with conn.cursor() as cursor:
+    cursor.execute(query)
+    results = cursor.fetchall()
+
+# for r in results:
+for r in results:
+    print(r[3])
+    pp = json.loads(r[2])
+    if r[0] < 30:  # rank 為屬以上
+        formatted_name = pp.get('latin_name')
+    elif r[0] == 30:  # rank 為屬
+        if r[1] == 2 and pp.get('is_hybrid_formula'):  # 命名規約為植物且為雜交
+            formatted_name = f"× <i>{pp.get('latin_name')}</i>"
+        else:
+            formatted_name = f"<i>{pp.get('latin_name')}</i>"
+    elif r[0] == 34:  # rank 為種
+        if r[1] == 2 and pp.get('is_hybrid_formula'):  # 命名規約為植物且為雜交
+            formatted_name = f"<i>{pp.get('latin_genus')}</i> × <i>{pp.get('latin_s1')}</i>"
+        else:
+            formatted_name = f"<i>{pp.get('latin_genus')} {pp.get('latin_s1')}</i>"
+    elif r[0] > 34:  # 種下 & 種下下
+        if r[1] == 1:  # 命名規約為動物
+            count = 0
+            for l in pp.get('species_layers'):
+                if count == 0:  # 種下rank不顯示
+                    formatted_name = f"<i>{pp.get('latin_genus')} {pp.get('latin_s1')} {l.get('latin_name')}</i>"
+                else:  # 種下下rank需顯示
+                    formatted_name += f" {l.get('rank_abbreviation')} <i>{l.get('latin_name')}</i>"
+                count += 1
+        else:  # 命名規約為植物
+            formatted_name = f"<i>{pp.get('latin_genus')} {pp.get('latin_s1')}</i>"
+            for l in pp.get('species_layers'):
+                formatted_name += f" {l.get('rank_abbreviation')} <i>{l.get('latin_name')}</i>"
+    query = f"INSERT INTO api_names (taxon_name_id, name_with_tag) VALUES (%s, %s);"
+    with conn.cursor() as cursor:
+        cursor.execute(query, (r[3], formatted_name))
+        conn.commit()
+
+# 雜交組合最後處理（要等學名已經建立）
+query = "WITH view as (SELECT tnhp.taxon_name_id, an.name_with_tag FROM taxon_name_hybrid_parent tnhp \
+         JOIN api_names an ON tnhp.parent_taxon_name_id = an.taxon_name_id \
+         ORDER BY tnhp.order) \
+         SELECT taxon_name_id, group_concat(name_with_tag SEPARATOR ' × ') FROM view \
+         GROUP BY taxon_name_id"
+
+conn = pymysql.connect(**db_settings)
+with conn.cursor() as cursor:
+    cursor.execute(query)
+    results = cursor.fetchall()
+for r in results:
+    query = f"INSERT INTO api_names (taxon_name_id, name_with_tag) VALUES (%s, %s);"
+    with conn.cursor() as cursor:
+        cursor.execute(query, (r[0], r[1]))
+        conn.commit()
+
+# name with HTML tag
+# 動物命名規約
+# 1. 屬以上抓latin name 且不用斜體
+# 2. 屬抓latin name且斜體
+# 3. 種抓 latin genus & latin s1 且斜體
+# 4. 種下 latin genus & latin s1 & latin s2 且斜體
+# 5. 種下下 latin genus & latin s1 & latin s2 & s3_rank & latin s3 **rank不斜體
+# 6. 雜交組合 hybrid parent1 × hybrid parent2
+
+
+# 植物命名規約
+# 1. 屬以上抓latin name 且不用斜體
+# 2. 屬抓latin name且斜體
+# 2-1. 屬且雜交 × latin name且斜體
+# 3. 種抓 latin genus & latin s1 且斜體
+# 3-1. 種且雜交 latin genus × latin s1
+# 4. 種下 latin genus & latin s1 & s2_rank  & latin s2 且斜體 **rank不斜體
+# 5. 種下下 latin genus & latin s1 & s2_rank & latin s2 & s3_rank & latin s3 **rank不斜體
+# 6. 雜交組合 hybrid parent1 × hybrid parent2
+
+
+# TODO name with tag 的更新流程 -> 在每次更新taxon的時候處理
+
+# TODO 如果未來原本是種下的上階層是有效的種，但改為無效，要怎麼更新？
+
+# TODO 這樣存資料表會不會太大？
