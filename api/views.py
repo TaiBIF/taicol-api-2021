bfe0f28c57488adb8c922eeac62f7dbfdd4c48bf
jinyinglee <45194511+jinyinglee@users.noreply.github.com>
2024-12-13 17:05:20 +0800

    ref API order

--- api/views.py
+++ api/views.py
@@ -599,14 +599,30 @@ class ReferencesView(APIView):
                          LEFT JOIN api_citations c ON ru.reference_id = c.reference_id \
                          WHERE ru.id = %s  AND ru.status != '' \
                          AND ru.is_title != 1 AND ru.deleted_at IS NULL"  # 不給backbone
-# AND r.type != 4
+                # AND r.type != 4
                 with conn.cursor() as cursor:
                     cursor.execute(query, (usage_id,))
                     df = pd.DataFrame(cursor.fetchall(), columns=['usage_id', 'name_id', 'reference_id', 'reference_type', 'publish_year', 'citation', 'usage_status', 'accepted_name_id', 'indications', 'is_in_taiwan', 'is_endemic', 'alien_type', 'is_deleted'])
+            
+            if name_id:
+                query = f"SELECT c.reference_id, CONCAT_WS(' ' , c.author, c.content), r.type, r.publish_year \
+                        FROM taxon_names tn \
+                        JOIN api_citations c ON tn.reference_id = c.reference_id    \
+                        JOIN `references` r ON tn.reference_id = r.id    \
+                        WHERE tn.id = %s AND tn.reference_id IS NOT NULL AND tn.deleted_at IS NULL AND tn.is_publish = 1"
+                with conn.cursor() as cursor:
+                    cursor.execute(query, (name_id, ))
+                    results = cursor.fetchall()
+                    for r in results:
+                        if r[0] not in df.reference_id.to_list():
+                            # 這邊不會有backbone
+                            df = pd.concat([df, pd.DataFrame([{'name_id': int(name_id), 'reference_id': r[0], 'reference_type': r[2], 'publish_year': int(r[3]), 'citation': r[1], 'usage_status': 'accepted'}])], ignore_index=True)
 
             if len(df):
                 df = df.replace({np.nan: None, 'null': None})
                 df['publish_year'] = df['publish_year'].apply(lambda x: int(x) if x else None)
+                df['reference_order'] = df['reference_type'].apply(lambda x: custom_reference_type_order[x])
+                df = df.sort_values('publish_year', ascending=False).sort_values('reference_order')
                 df.loc[df.reference_type==4, 'reference_id'] = None
                 df.loc[df.reference_type==4, 'publish_year'] = None
                 df.loc[df.reference_type==4, 'citation'] = 'TaiCOL Backbone'
@@ -626,28 +642,23 @@ class ReferencesView(APIView):
                                 
                 df = df.replace({np.nan: None, '': None})
                 df['reference_id'] = df['reference_id'].replace({np.nan: 0}).astype('int64').replace({0: None})
-
+                df = df.drop(columns=['reference_order'])
                 data = df.to_dict('records')
+
+                new_data = []
+                for d in data:
+                    if not d.get('usage_id'):
+                        poping_keys = [k for k in df.keys() if k not in [ 'name_id', 'reference_id', 'reference_type', 'publish_year', 'citation',  'usage_status']]
+                        for p in poping_keys:
+                            d.pop(p)
+                    new_data.append(d)
 
             # 加上原始文獻 status補上accepted
 
-            if name_id:
-                query = f"SELECT c.reference_id, CONCAT_WS(' ' , c.author, c.content), r.type, r.publish_year \
-                        FROM taxon_names tn \
-                        JOIN api_citations c ON tn.reference_id = c.reference_id    \
-                        JOIN `references` r ON tn.reference_id = r.id    \
-                        WHERE tn.id = %s AND tn.reference_id IS NOT NULL AND tn.deleted_at IS NULL AND tn.is_publish = 1"
-                with conn.cursor() as cursor:
-                    cursor.execute(query, (name_id, ))
-                    results = cursor.fetchall()
-                    for r in results:
-                        if r[0] not in df.reference_id.to_list():
-                            # 這邊不會有backbone
-                            data.append({'name_id': int(name_id), 'reference_id': r[0], 'reference_type': reference_type_map[r[2]], 'publish_year': int(r[3]), 'citation': r[1], 'usage_status': 'accepted'})
             conn.close()
         
             response = {"status": {"code": 200, "message": "Success"},
-                        "info": {"total": len(df)}, "data": data}
+                        "info": {"total": len(df)}, "data": new_data}
         except Exception as er:
             print(er)
             response = {"status": {"code": 500, "message": "Unexpected Error"}}
